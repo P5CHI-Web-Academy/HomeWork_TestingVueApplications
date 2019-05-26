f37a6d53c7ccbfe4adf160210238d859a505759d
Leonid Koida <lkoida@straighterline.com>
2019-05-26 22:45:34 +0300

    added test to spiceItem component, test content form props, should pass

--- tests/unit/specs/components/SpiceItem.spec.js
+++ tests/unit/specs/components/SpiceItem.spec.js
@@ -15,6 +15,25 @@ describe('SpiceItem.vue', () => {
       }
     })
 
-    expect(wrapper.props()).toEqual({spice, order, index})
+    expect(wrapper.props()).toEqual({ spice, order, index })
+  })
+
+  test('render content from props', () => {
+    const spice = {
+      title: 'some title'
+    }
+    const order = 10
+    const index = 5
+
+    const wrapper = shallowMount(SpiceItem, {
+      propsData: {
+        spice,
+        order,
+        index
+      }
+    })
+
+    expect(wrapper.text()).toContain(spice.title)
+    expect(wrapper.text()).toContain(order)
   })
 })
