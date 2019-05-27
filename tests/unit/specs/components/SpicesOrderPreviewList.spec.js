4eeb46957e9d7e550c09962d4c8a9842140900f6
Leonid Koida <lkoida@straighterline.com>
2019-05-27 23:53:10 +0300

    added homework

---
+++ tests/unit/specs/components/SpicesOrderPreviewList.spec.js
@@ -0,0 +1,12 @@
+import { shallowMount } from '@vue/test-utils'
+import SpicesOrderPreviewList from '@/components/SpicesOrderPreviewList'
+
+// TODO homework
+describe.skip('SpicesOrderPreviewList', () => {
+  test.todo('check if it is renders correct amount of list items')
+  test.todo('move data fetching from store, using getters')
+  test.todo('create test and functionality for remove spice from list of preordered spices')
+
+  test.todo('check click `Add to Order` is triggered')
+  test.todo('check store for items in order')
+})
