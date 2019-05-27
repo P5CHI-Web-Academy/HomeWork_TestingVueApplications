5dc07b93e8b5559fbb5a0d0d61f70c8299848a86
Leonid Koida <lkoida@straighterline.com>
2019-05-27 23:17:59 +0300

    added missing import for styles

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import BootstrapVue from 'bootstrap-vue'
+import '@/assets/sass/index.scss'
 import store from './store'
 
 Vue.config.productionTip = false
