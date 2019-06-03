16ef7e72c2d039d4df3980924abf81cc39ff86fb
Masha Afonina <mashaafonina@yandex.ru>
2019-06-03 22:40:44 +0200

    Test that SpicesOrderPreviewList renders correct number of items

--- tests/unit/specs/components/SpicesOrderPreviewList.spec.js
+++ tests/unit/specs/components/SpicesOrderPreviewList.spec.js
@@ -1,12 +1,22 @@
-import { shallowMount } from '@vue/test-utils'
+import { mount, shallowMount } from '@vue/test-utils'
 import SpicesOrderPreviewList from '@/components/SpicesOrderPreviewList'
+import SpiceItem from '@/components/SpiceItem'
 
 // TODO homework
-describe.skip('SpicesOrderPreviewList', () => {
-  test.todo('check if it is renders correct amount of list items')
-  test.todo('move data fetching from store, using getters')
-  test.todo('create test and functionality for remove spice from list of preordered spices')
+describe('SpicesOrderPreviewList', () => {
+  test('check if it renders correct amount of list items', () => {
+    const inPreOrder = [{}, {}, {}]
+    const wrapper = shallowMount(SpicesOrderPreviewList, {
+      propsData: {
+        inPreOrder
+      }
+    })
+    expect(wrapper.findAll('div.b-list-group-item')).toHaveLength(inPreOrder.length)
+  })
 
-  test.todo('check click `Add to Order` is triggered')
-  test.todo('check store for items in order')
+  // test.todo('move data fetching from store, using getters')
+  // test.todo('create test and functionality for remove spice from list of preordered spices')
+
+  // test.todo('check click `Add to Order` is triggered')
+  // test.todo('check store for items in order')
 })
