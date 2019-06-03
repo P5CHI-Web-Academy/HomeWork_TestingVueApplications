de0a0c86b9a8b84b7e8cf2dd41ac3ebb868deb6f
Eduard Climov <klimovedik88@gmail.com>
2019-06-03 14:54:07 +0300

    - Implemented tests for Order

--- tests/unit/specs/views/Order.spec.js
+++ tests/unit/specs/views/Order.spec.js
@@ -1,11 +1,94 @@
-test.todo('check list of items')
-/*
-* TODO add some logic to price calculation, may be some discounts if order have
-*  more then 3 items, or if if order have more then 3 unique items, or if order have more
-*  then 3 unique items and some of items are duplicated add a discout for duplicated items
-*  any logic are welcome. And all this branches of logic should be tested))
-* */
-test.todo('add price for each item in collection calculate it and test')
-test.todo('add possibility and to remove items from order`and recalcualte all discounts`')
-test.todo('add possibility to increase amount of each item and recalcualte prices')
-test.todo('add possibility to clear order list')
+import {createLocalVue, mount, shallowMount} from "@vue/test-utils";
+import Vuex from "vuex";
+import Order from "../../../../src/views/Order";
+
+describe('Order', () => {
+  const inOrder = {
+    1:     {
+      title: 'title1',
+      img: 'img1',
+      description: 'description1',
+      shortDescription: 'shortDescription1',
+      price: 1.11,
+      quantity: 1
+    },
+  2: {
+    title: 'title2',
+      img: 'img2',
+    description: 'description2',
+    shortDescription: 'shortDescription2',
+    price: 2.22,
+    quantity: 1
+  }
+  }
+
+
+  let localVue = createLocalVue()
+  localVue.use(Vuex)
+
+  let store
+  let getters
+  let actions
+  beforeEach(() => {
+    getters = {
+      getOrderSpices: () => inOrder,
+      isOrderDiscount: () => true
+    }
+
+    actions = {
+      clearOrder: jest.fn(() => Promise.resolve([])),
+      updateQuantity: jest.fn(() => Promise.resolve([]))
+    }
+
+    store = new Vuex.Store({
+      getters,
+      actions
+    })
+  })
+
+
+  test('check store for items in order', () =>
+  {
+    const wrapper = mount(Order, { store, localVue })
+
+    expect(wrapper.findAll('.list-group-item')).toHaveLength(Object.keys(inOrder).length)
+  })
+
+
+  test('action `updateQuantity` is triggered when clicking corresponding buttons', async () => {
+    const wrapper = mount(Order, { store, localVue })
+
+    wrapper.find('.btn-warning').trigger('click')
+    expect(actions.updateQuantity).toHaveBeenCalled()
+
+    wrapper.find('.btn-success').trigger('click')
+    expect(actions.updateQuantity).toHaveBeenCalled()
+  })
+
+
+  test('`clearOrder` is called when [clear] button is clicked', async () => {
+    const wrapper = mount(Order, { store, localVue })
+
+    wrapper.find('.btn-danger').trigger('click')
+    expect(actions.clearOrder).toHaveBeenCalled()
+  })
+
+
+  test('`discount` message is rendered, when there is discount', async () => {
+    const wrapper = mount(Order, { store, localVue })
+    expect(wrapper.text()).toContain('DISCOUNT')
+  })
+
+
+  test('`discount` message is NOT rendered, when there is no discount', async () => {
+    let getters = {
+      getOrderSpices: () => {},
+      isOrderDiscount: () => false
+    }
+    let store = new Vuex.Store({
+      getters: getters
+    })
+    const wrapper = mount(Order, { store, localVue })
+    expect(wrapper.text()).not.toContain('DISCOUNT')
+  })
+})
